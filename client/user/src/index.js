bef45fb22ba14b3531cd3af4cfa5aab27ef5d8a2
Dexter <shalimovvlad@gmail.com>
2019-04-18 18:45:33 +0300

    redux-persist

--- client/user/src/index.js
+++ client/user/src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom';
 import {Router} from 'react-router';
 import {createBrowserHistory} from "history";
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/lib/integration/react';
 
-import store from './store';
+import {store, persistor} from './store';
 import Routes from './routes';
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={history}>
-            <Routes/>
-        </Router>
+        <PersistGate persistor={persistor}>
+            <Router history={history}>
+                <Routes/>
+            </Router>
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
